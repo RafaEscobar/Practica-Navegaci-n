5ad1935432bac26dbc2569dcb2772258e48b082e
Rafael Escobar Gutierrez <Rafael.EG.code@gmail.com>
2022-10-27 15:28:38 -0500

    Aplicando uso de funcional-component

--- src/components/InitMessageComponent.tsx
+++ src/components/InitMessageComponent.tsx
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-interface MyTxt {
-    text1: string
-}
-
-export class InitMessageComponent extends Component < MyTxt > {
-
-    constructor(props: any){
-        super(props);
-    }
-
-    render(){
-        return(
-            <View style={ styles.container }>
-                <Text style={styles.text}> {this.props.text1} </Text>
-            </View>
-        )
-    }
-    
-}
+export const InitMessageComponent = ({ text1 }:any) => (
+        <View style={ styles.container }>
+            <Text style={styles.text}> {text1} </Text>
+        </View>
+)
 
 const styles = StyleSheet.create({
 
--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -3,6 +3,6 @@ import { InitMessageComponent } from '../components/InitMessageComponent';
 
 export const InfoPage = () => {
   return (
-    <InitMessageComponent text1='Información [Esto no deberia aparecer]...' />
+    <InitMessageComponent text1='Información...' />
   )
 }
